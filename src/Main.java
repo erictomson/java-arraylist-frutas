462a4b43ff1f67805c7b6c58f44fcbd94025526d
erictomson <erictomson@gmail.com>
2021-10-06 23:09:07 -0300

    Adicionando comentários

--- src/Main.java
+++ src/Main.java
@@ -17,8 +17,11 @@ public class Main {
 
         // Criando string para exibir a lista
         String saida = "";
+        // Imprimindo elementos do ArrayList
         saida += "Frutas da Lista: " + listaFrutas.toString();
+        // Verificando o tamanho do ArrayList
         saida += "\nTotal de Frutas na lista: " + listaFrutas.size();
+        // Pesquisando o ArrayList
         saida += "\nA lista possui pera? " + listaFrutas.contains("pera");
         saida += "\nA lista possui caju? " + listaFrutas.contains("caju");
 
@@ -26,12 +29,16 @@ public class Main {
         listaFrutas.remove("uva");
 
         // Adicionando a lista atualizada
+        // Verificando o tamanho do ArrayList
         saida += "\n\nTotal de Frutas da lista após remover uva: " + listaFrutas.size();
+        // Imprimindo elementos do ArrayList
         saida += "\nFrutas na lista após remover uva: " + listaFrutas.toString();
+        // Pesquisando a posição de um elemento no ArrayList
         saida += "\nÍndice da fruta caju na lista: " + listaFrutas.indexOf("caju");
 
         // Configurando a TextArea
         JTextArea saidaArea = new JTextArea();
+        // Atribuindo a string com o conteúdo do ArraList à TextArea
         saidaArea.setText(saida);
 
         // Exibindo ArrayList com caixa de diálogo
